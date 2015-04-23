2acb88884aeab6678b1e454bbfc68da258bd75c0
Vison <kk801030@gmail.com>
2015-04-23 16:40:52 +0800

    added basic table view

--- funnyPie/FirstViewController.swift
+++ funnyPie/FirstViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let storys = ["Sport", "Entertainment", "Work"]
+    let cellIdentifier = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,23 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: UITableView Methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.storys.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = storys[row]
+        
+        return cell
+    }
+    
 }
 
