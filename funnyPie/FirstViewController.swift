3d989e538855b04b19e608c2fc39d99e59c71530
Vison <kk801030@gmail.com>
2015-04-23 15:51:13 +0800

    Initial Commit

---
+++ funnyPie/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  funnyPie
+//
+//  Created by 李哲維 on 2015/4/23.
+//  Copyright (c) 2015年 NTU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
