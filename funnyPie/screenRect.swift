22cfc13ef433421b1bbf62fa28b5b7bbe9ccfe3e
Vison <kk801030@gmail.com>
2015-04-26 14:31:45 +0800

    added screenRect

---
+++ funnyPie/screenRect.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  funnyPie
+//
+//  Created by 李哲維 on 2015/4/23.
+//  Copyright (c) 2015年 NTU. All rights reserved.
+//
+
+import UIKit
+
+class screenRect {
+    
+    static let width: CGFloat = UIScreen.mainScreen().bounds.width
+    static let height: CGFloat = UIScreen.mainScreen().bounds.size.height
+
+    
+}
